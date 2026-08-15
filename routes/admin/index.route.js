a69490bc1376591e2af0014444a32b2dbfea59ff
ShihabMahmod <shihabmahmod58@gmail.com>
2026-08-15 12:19:25 +0600

    brand feature done

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,8 +1,11 @@
 const Router = require('express').Router();
 
+
+Router.use('/brand',      require('./brand.route'));
 Router.use('/category',   require('./category.route'));
 
 
 Router.use('/auth',      require('./auth.route'));
+
 
 module.exports = Router;
