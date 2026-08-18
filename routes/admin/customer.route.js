0646fa4170787d7b2c70c4522732bf0b4a5941bc
ShihabMahmod <shihabmahmod58@gmail.com>
2026-08-18 21:21:13 +0600

    create customer route

---
+++ routes/admin/customer.route.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const AdminCustomerController = require('../../controllers/admin/customer.controller');
+const { adminProtect } = require('../../middleware/auth.middleware');
+const { objectId } = require('../../validations/shared');
+const validate = require('../../middleware/validate');
+const { z } = require('zod');
+
+router.use(adminProtect);
+
+const idSchema = z.object({ params: z.object({ id: objectId }) });
+
+router.get('/',                                             AdminCustomerController.index);
+router.get('/:id',       validate(idSchema),               AdminCustomerController.show);
+router.patch('/:id/toggle-status', validate(idSchema),     AdminCustomerController.toggleStatus);
+router.get('/:id/orders',validate(idSchema),               AdminCustomerController.orderHistory);
+
+module.exports = router;
